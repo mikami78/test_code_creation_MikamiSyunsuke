2803b86594a0b2ab3db5dcab3ed68b985593454f
mikami78 <s-mikami@nexis.co.jp>
2026-09-18 17:39:47 +0900

    fix:Case1.javaログイン画面への遷移 修正, Case2.java受講生 ログイン 認証失敗 修正

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case01.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case01.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.Assert.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -31,11 +32,36 @@ public class Case01 {
 		closeDriver();
 	}
 
+	private static final String url = "http://localhost:8080/lms/";
+	private static final String title = "ログイン | LMS";
+
 	@Test
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		// 下記URLのページにアクセスさせる
+		webDriver.get(url);
+
+		// エビデンス(スクリーンショット)を取る
+		getEvidence(new Object() {
+		});
+
+		// URLがLMSのログイン画面のURLであるかを照合させる
+		assertEquals(url, webDriver.getCurrentUrl());
 	}
 
+	@Test
+	@Order(2)
+	@DisplayName("テスト02 ログイン画面のタイトルの確認")
+	void test02() {
+		// 下記URLのページにアクセスさせる
+		webDriver.get(url);
+
+		// エビデンス(スクリーンショット)を取る
+		getEvidence(new Object() {
+		});
+
+		// ログイン画面のタイトルを確認させる
+		assertEquals(title, webDriver.getTitle());
+	}
 }
