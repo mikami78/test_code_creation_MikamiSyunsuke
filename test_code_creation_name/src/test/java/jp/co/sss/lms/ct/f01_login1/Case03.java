d3c2d90b241379cb6efb583b3e95c47056162f00
mikami78 <s-mikami@nexis.co.jp>
2026-09-24 14:41:28 +0900

    Case3,4をコミットします

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case03.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case03.java
@@ -1,6 +1,9 @@
 package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.Assert.*;
+
+import java.time.Duration;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +12,10 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
  * 結合テスト ログイン機能①
@@ -31,18 +38,56 @@ public class Case03 {
 		closeDriver();
 	}
 
+	private static final String url = "http://localhost:8080/lms/";
+	private static final String title = "ログイン | LMS";
+
 	@Test
 	@Order(1)
-	@DisplayName("テスト01 トップページURLでアクセス")
+	@DisplayName("テスト01 ログイン画面のタイトルの確認")
 	void test01() {
-		// TODO ここに追加
+		// 下記URLのページにアクセスさせる
+		webDriver.get(url);
+
+		// エビデンス(スクリーンショット)を取る
+		getEvidence(new Object() {
+		});
+
+		// ログイン画面のタイトルを確認させる
+		assertEquals(title, webDriver.getTitle());
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+		// 要素を取得させる
+		final WebElement loginId = webDriver.findElement(By.id("loginId"));
+		final WebElement password = webDriver.findElement(By.id("password"));
+		final WebElement loginButton = webDriver.findElement(By.cssSelector("input[type='submit']"));
+		final By welcomeMessage = By.cssSelector("small");
+
+		// ログインIDフォームをクリアしてからログインIDを入力する
+		loginId.clear();
+		loginId.sendKeys("StudentAA01");
+
+		// パスワードフォームをクリアしてからパスワードを入力する
+		password.clear();
+		password.sendKeys("StudentAA001");
+
+		// ログインボタンをクリック
+		loginButton.click();
+
+		// ログインメッセージが画面上に表示されるまで待つ
+		final WebElement loginElement = new WebDriverWait(
+				webDriver, Duration.ofSeconds(5))
+						.until(ExpectedConditions.visibilityOfElementLocated(welcomeMessage));
+
+		// エビデンス(スクリーンショット)を取る
+		getEvidence(new Object() {
+		});
+
+		// ログインメッセージが画面上に表示されているかを確認させる
+		assertTrue(loginElement.getText().contains("ようこそ受講生ＡＡ１さん"));
 	}
 
 }
